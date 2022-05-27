00d102b58af9b9a8ac84e5258cbcac750fa0ca01
Elias <eliaskibret2013n2@gmail.com>
2022-05-27 22:29:02 +0300

    setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Register, Landing, Error, ProtectedRoute } from './pages'
-import {
-  AllJobs,
-  Profile,
-  SharedLayout,
-  Stats,
-  AddJob,
-} from './pages/dashboard'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <ProtectedRoute>
-              <SharedLayout />
-            </ProtectedRoute>
-          }
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <Route index element={<Stats />} />
-          <Route path='all-jobs' element={<AllJobs />} />
-          <Route path='add-job' element={<AddJob />} />
-          <Route path='profile' element={<Profile />} />
-        </Route>
-        <Route path='/register' element={<Register />} />
-        <Route path='/landing' element={<Landing />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-    </BrowserRouter>
-  )
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
