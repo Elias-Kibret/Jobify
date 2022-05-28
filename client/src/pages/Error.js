007c88acbabf5cc4267fa921441bc8173ffbaf11
Elias <eliaskibret2013n2@gmail.com>
2022-05-28 22:55:05 +0300

    register page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,6 +1,15 @@
+import {Link} from 'react-router-dom'
+import img from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
+
 const Error = () => {
   return (
-    <div>Error</div>
+    <Wrapper className='full-page'>
+        <img src={img} alt="not-found"/>
+        <h3>ohh! page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to='/'>Back Home</Link>
+    </Wrapper>
   )
 }
 
