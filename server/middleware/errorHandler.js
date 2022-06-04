1fd09c74a8db3692024f8a8d08d410ace56c1a52
Elias <eliaskibret2013n2@gmail.com>
2022-06-04 12:00:01 +0300

    handle email uniquness

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -10,6 +10,10 @@ export const errorHandler=(err,req,res,next)=>{
         defaultError.statusCode=StatusCodes.BAD_REQUEST
         defaultError.msg=Object.values(err.errors).map((item)=>item.message).join(',')
     }
+    if(err.code && err.code===11000){
+        defaultError.statusCode=StatusCodes.BAD_REQUEST
+        defaultError.msg=`${Object.keys(err.keyValue)} field has to be unique`
+    }  
     //  res.status(defaultError.statusCode).json({msg:err})
      res.status(defaultError.statusCode).json({msg:defaultError.msg})
 
