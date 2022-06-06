d233eebd54c8bde09a6a7d02402419769fb8151f
Elias <eliaskibret2013n2@gmail.com>
2022-06-06 16:37:23 +0300

    restructure folder && connect frontend and backend

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,10 @@
 import {useEffect} from 'react'
+
 const Dashboard = () => {
   const fetchData =async ()=>{
     try {
-      const response=await fetch('http://localhost:5000/')
+      const response=await fetch('../../public/data.json')
+      // const response=await fetch('http://localhost:5000/')
       const data=await response.json()
       console.log(data)
     } catch (error) {
