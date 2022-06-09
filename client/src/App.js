c5d4431e9a0fab82997c1de83d61b0b7a5fe3336
Elias <eliaskibret2013n2@gmail.com>
2022-06-09 15:53:50 +0300

    nested route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import {Register,Landing,Error} from './pages/index'
+import {
+  AddJob,
+  Profile,
+  SharedLayout,
+  AllJobs,
+  Stats
+} from "./pages/DashBoard/index.js"  
 function App() {
   return (
-    <BrowserRouter className="App">
-      
+    <BrowserRouter className="App">  
       <Routes>
-        <Route path="/" element={<div>DashBoard</div>}/>
+        <Route path="/" element={<SharedLayout/>} >
+          <Route indexli element={<Stats/>}/>
+          <Route path="all-jobs" element={<AllJobs/>}/>
+          <Route path="add-jobs" element={<AddJob/>}/>
+          <Route path="profile" element={<Profile/>}/>
+        </Route>
         <Route path="/register" element={<Register/>}/>
         <Route path="/landing" element={<Landing/>}/>
         <Route path="*" element={<Error/>}/>
       </Routes>
-  
-
     </BrowserRouter>
   );
 }
