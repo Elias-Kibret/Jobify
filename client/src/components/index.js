a9920b96e0fa525b80be21e99bb49390725ee02e
Elias <eliaskibret2013n2@gmail.com>
2022-06-09 17:32:22 +0300

    shared layout

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,10 +1,16 @@
 import NewLogo from '../components/NewLogo'
 import FormRow from './FormRow'
 import Alert from './Alert'
+import Navbar from './Navbar'
+import BigSidebar from './BigSidebar'
+import SmallSidebar from './SmallSidebar'
 
 export {
     NewLogo,
     FormRow,
-    Alert
+    Alert,
+    Navbar,
+    BigSidebar,
+    SmallSidebar
 
 }
