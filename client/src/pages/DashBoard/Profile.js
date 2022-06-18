5f74f69c931a980f2c31b49d702c7f53e0b073d4
Elias <eliaskibret2013n2@gmail.com>
2022-06-18 18:03:52 +0300

    profile form

--- client/src/pages/DashBoard/Profile.js
+++ client/src/pages/DashBoard/Profile.js
@@ -23,7 +23,27 @@ const Profile = () => {
   
   return (
     <Wrapper>
+       <form className='form' onSubmit={handleSubmit}>
+         <h3>Profile</h3>
+         {showAlert&&<Alert/>}
+         <div className="form-center">
+            <FormRow 
+               type="text"
+               name="name" 
+               value={name} 
+               handleChange={(e)=>{
+              setName(e.target.value)}}
+              />
 
+            <FormRow 
+                 type="text"
+                 name="name" 
+                 value={name} 
+                 handleChange={(e)=>{
+                setName(e.target.value)}}
+              />
+         </div>
+         </form>             
     </Wrapper>
   )
 }
